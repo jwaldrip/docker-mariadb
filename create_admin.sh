6fff2c562d43a675f4db61e62d54f3dcd8995775
Jason Waldrip <jason@waldrip.net>
2014-08-19 22:37:11 -0600

    Update create_admin.sh

--- create_admin.sh
+++ create_admin.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-if [ -f /.mysql_admin_created ]; then
-	echo "MySQL 'admin' user already created!"
-	exit 0
-fi
+srv_ready?(){
+  set -e
+  mysql -uroot -e "show databases";
+}
 
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
+# Start Server
+/usr/bin/mysqld_safe &> /dev/null &
+mysqld_pid=$!
 
+# Wait for server
+while ! srv_ready? ; do printf '.' ; done
+
+# Create Admin
 echo "=> Creating MySQL user: '$MYSQL_USER' with password: '$MYSQL_PASS'"
-RET=1
-while [[ RET -ne 0 ]]; do
-	sleep 5
-	mysql -uroot -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS'"
-	RET=$?
-done
-
+mysql -uroot -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION"
 
-mysqladmin -uroot shutdown
-
+# Kill Server for Safe Measure
+kill $mysql_pid
 echo "=> Done!"
-touch /.mysql_admin_created
-
-echo "========================================================================"
-echo "You can now connect to this MySQL Server using:"
-echo ""
-echo "    mysql -u$MYSQL_USER -p$MYSQL_PASS -h<host> -P<port>"
-echo ""
-echo "Please remember to change the above password as soon as possible!"
-echo "MySQL user 'root' has no password but only allows local connections"
-echo "========================================================================"
